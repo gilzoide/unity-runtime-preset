0169ef2d2f1cab5bfd41f869310181e4084135ed
gilzoide <gilzoide@gmail.com>
2024-01-05 19:49:25 -0300

    Add context menu for creating runtime presets from existing assets

--- Editor/ContextMenuItems.cs
+++ Editor/ContextMenuItems.cs
@@ -6,7 +6,7 @@ namespace Gilzoide.RuntimePreset.Editor
     public static class ContextMenuItems
     {
         [MenuItem("CONTEXT/MonoBehaviour/Create Runtime Preset", priority = 10000)]
-        private static void CreateRuntimePreset(MenuCommand command)
+        private static void CreateRuntimePresetFromMonoBehaviour(MenuCommand command)
         {
             if (command.context is MonoBehaviour component)
             {
@@ -18,6 +18,25 @@ namespace Gilzoide.RuntimePreset.Editor
 
                 RuntimePreset newPreset = ScriptableObject.CreateInstance<RuntimePreset>();
                 newPreset.TargetType = component.GetType();
+                // TODO: fill new preset with existing values
+                AssetDatabase.CreateAsset(newPreset, path);
+                EditorGUIUtility.PingObject(newPreset);
+            }
+        }
+
+        [MenuItem("CONTEXT/ScriptableObject/Create Runtime Preset", priority = 10000)]
+        private static void CreateRuntimePresetFromScriptableObject(MenuCommand command)
+        {
+            if (command.context is ScriptableObject obj)
+            {
+                string path = EditorUtility.SaveFilePanelInProject("Create Runtime Preset", obj.GetType().Name + "_preset", "asset", "");
+                if (string.IsNullOrWhiteSpace(path))
+                {
+                    return;
+                }
+
+                RuntimePreset newPreset = ScriptableObject.CreateInstance<RuntimePreset>();
+                newPreset.TargetType = obj.GetType();
                 // TODO: fill new preset with existing values
                 AssetDatabase.CreateAsset(newPreset, path);
                 EditorGUIUtility.PingObject(newPreset);
