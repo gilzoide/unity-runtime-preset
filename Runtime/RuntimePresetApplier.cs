8b71218133159d4eb0cf345da5fc9a8782692bed
gilzoide <gilzoide@gmail.com>
2024-01-06 10:16:37 -0300

    Add target object to RuntimePresetApplier

--- Runtime/RuntimePresetApplier.cs
+++ Runtime/RuntimePresetApplier.cs
@@ -6,6 +6,7 @@ namespace Gilzoide.RuntimePreset
     public class RuntimePresetApplier : MonoBehaviour
     {
         [SerializeField] protected List<RuntimePreset> _presets;
+        [SerializeField] protected Object _target;
         [SerializeField] protected PresetApplicationEvent _applyAt = PresetApplicationEvent.Awake;
         [SerializeField] protected internal bool _destroyAfterImport = false;
 
@@ -43,9 +44,10 @@ namespace Gilzoide.RuntimePreset
         [ContextMenu("Apply Preset")]
         public void Apply()
         {
+            Object target = _target ? _target : gameObject;
             foreach (RuntimePreset runtimePreset in _presets)
             {
-                runtimePreset.ApplyTo(gameObject);
+                runtimePreset.ApplyTo(target);
             }
         }
     }
